dc41861fbd183b0fbc35de1790e3f516a424e3d3
Kylo <me@kylo.io>
2015-02-28 23:09:45 -0600

    Better performance on links
    Sidebar ones still don't go blue!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,49 +1,67 @@
-console.log('\'Allo \'Allo!');
-
-
 var paths = ['', 'home', 'about', 'sponsors','donate', 'first', 'media', 'resources', 'map'];
 
-loadPage(null);
+var content = $("#content");
+var partial = 'home';
 
-$('.ajax').on('click', function()  {
-	loadPage($(this).attr('href'));
+$(document).on('click', '.ajax', function()  {
+	loadPage($(this).attr('href').substr(1));
 });
 
 function loadPage(page) {
-	var hash = window.location.hash
+  var path = page;
 
-	if (page != null) {
-		hash = page;
-	}
+  var to = null;
 
-	var path = hash.substr(1);
+  if (page.indexOf("\.") > 0)
+    return;
 
-	var index = jQuery.inArray(path, paths);
+  $.each(paths, function(key, value) {
+    if (value.length == 0)
+      return;
+    if (path.match("^" + value)) {
+      to = value;
+    }
+  });
 
-	if (index == -1) {
-		return;
-	}
+  if (to == null)
+    return;
 
-	if (path == '') {
-		path = 'home';
-	}
+  partial = to;
 
-	$('#content').load('/partials/' + path + '.html');
+  $.get("/partials/" + partial + ".html", function(data) {
+    $("#content").html(data);
+    updateLinks();
+  });
+}
 
-	$('.ajax').each(function() {
-		var thisPath = $(this).attr('href').substr(1);
-		if (thisPath != path) {
-			$(this).parent().removeClass('active');
-		}
-		else {
-			$(this).parent().addClass('active');
-		}
-	});
+$(document).on("scroll", function() {
+  console.log($(".about-members").attr("href"));
+})
+
+function updateLinks() {
+  var path = getPath();
+  $('.ajax').each(function() {
+    var thisPath = $(this).attr('href').substr(1);
+    if (thisPath == partial || thisPath == path) {
+      $(this).addClass('active');
+      $(this).parent().addClass('active');
+    }
+    else {
+      $(this).removeClass('active');
+      $(this).parent().removeClass('active');
+    }
+  });
+  $('.scrollspy').scrollspy()
+}
+
+function getPath() {
+  var hash = window.location.hash
+  return hash.substr(1);
 }
 
 $(".rslides").responsiveSlides({
   auto: true,             // Boolean: Animate automatically, true or false
-  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  speed: 500,             // Integer: Speed of the transition, in milliseconds
   timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
   pager: false,           // Boolean: Show pager, true or false
   nav: false,             // Boolean: Show navigation, true or false
